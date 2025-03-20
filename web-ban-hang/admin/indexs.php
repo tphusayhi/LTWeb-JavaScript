d9cc8bb2886af86f6c6edf840999b6511566b5f0
Nha <vannha.12345k2@gmail.com>
2025-03-20 15:02:14 +0700

    add

---
+++ web-ban-hang/admin/indexs.php
@@ -0,0 +1,35 @@
+<?php
+     include "views/header/header.php";
+     
+     
+    if(isset($_GET['act'])){   
+        switch($_GET['act']){   
+           case 'danhmuc' :
+            // nhận yêu cầu xử lý và lấy danh sách danh mục
+               
+               $kq=getall_danhmuc();
+               include "views/danhmuc.php";
+               break;
+           default:
+           
+           
+
+       }
+    }else{
+       include "";
+    }
+
+    
+    
+
+
+    
+
+
+
+
+
+    include "views/footer/footer.php";
+     
+
+?>
---
+++ web-ban-hang/admin/views/register/database.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_database";
+
+
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+
+
+
+
+?>
